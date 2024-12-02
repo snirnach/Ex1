6b0f5123dc9a23a245931a63276797dfc126e459
snirnach <snir.nach@gmail.com>
2024-12-02 14:10:32 +0200

    version 1

---
+++ src/Ex1Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Intro2CS, Ex1 - very basic "main template"
+ * Make sure your implementation of this main performs as the Ex1Sol.jar solution implement all needed functions.
+ *
+ */
+public class Ex1Main {
+    public static void main(String[] args) {
+        System.out.println(Ex1.int2Number(34,16));
+
+//        Scanner sc = new Scanner(System.in);
+//        String num1 = "", num2="", quit = "quit";
+//        while (!num1.equals(quit) && !num2.equals(quit)) {
+//            System.out.println();
+//            System.out.println("Ex1 class solution:");
+//            System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
+//            num1 = sc.next();
+//            if (!num1.equals("quit")) {
+//                System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
+//
+//            }
+//        }
+//        System.out.println("quiting now...");
+    }
+}
---
+++ src/Ex1Test.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * This JUnit class represents a very partial test class for Ex1.
+ * Make sure you complete all the needed JUnits
+ */
+public class Ex1Test {
+        @Test
+        void computeNumberTest() {
+            String s2 = "1011b2";
+            int v = Ex1.number2Int(s2);
+            assertEquals(v,11);
+            String s10 = "1011bA";
+            v = Ex1.number2Int(s10);
+            s2 = Ex1.int2Number(v,2);
+            int v2 = Ex1.number2Int(s2);
+            assertEquals(v,v2);
+            assertTrue(Ex1.equals(s10,s2));
+        }
+
+        @Test
+        void isBasisNumberTest() {
+            String[] good = {"1", "1b2", "01b2", "123bA", "ABbG", "0bA"};
+            for(int i=0;i<good.length;i=i+1) {
+                boolean ok = Ex1.isNumber(good[i]);
+                assertTrue(ok);
+            }
+            String[] not_good = {"b2", "2b2", "1G3bG", " BbG", "0bbA", "abB", "!@b2", "A", "1bb2"};
+            for(int i=0;i<not_good.length;i=i+1) {
+                boolean not_ok = Ex1.isNumber(not_good[i]);
+                assertFalse(not_ok);
+            }
+        }
+        @Test
+        void int2NumberTest() { //check if base<2 or base>16 or num<0 return empty string
+            assertEquals("",Ex1.int2Number(34,17));
+            assertEquals("",Ex1.int2Number(34,1));
+            assertEquals("",Ex1.int2Number(-1,5));
+        }
+    @Test
+    void int2NumberTest1() {
+        assertEquals("22bG",Ex1.int2Number(34,16));
+        assertEquals("10110b2",Ex1.int2Number(22,2));
+        }
+        @Test
+        void maxIndexTest() {
+
+        }
+
+        // Add additional test functions - test as much as you can.
+    }
